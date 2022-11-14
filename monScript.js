4945abcbcf2b7123567ea3ab60e75ce16c9051ea
SeleneTononETPA <selene.t.etpa@gmail.com>
2022-11-14 20:03:16 +0100

    ajout affichage action

--- monScript.js
+++ monScript.js
@@ -1,7 +1,9 @@
 maDiv = document.getElementById("maDiv");
 valeurResultat = document.getElementById("valeurResultat");
+afficheAction = document.getElementById("afficheAction");
 
 maDiv.onclick = function() {
     valeurResultat.innerHTML = valeurResultat.innerHTML+10;
-  }
+    afficheAction.innerHTML = "J'ai ajouté 10 !";
+}
 // Exercice : débuggez ce script :)
