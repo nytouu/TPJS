a1667ccb4d6a0f2c620cbe77cdbaf9e7298383dc
SeleneTononETPA <selene.t.etpa@gmail.com>
2022-11-14 19:54:53 +0100

    init script tp

---
+++ monScript.js
@@ -0,0 +1,7 @@
+maDiv = document.getElementById("maDiv");
+valeurResultat = document.getElementById("valeurResultat");
+
+maDiv.onclick = function() {
+    valeurResultat.innerHTML = valeurResultat.innerHTML+10;
+  }
+// Exercice : débuggez ce script :)
