9bd34eb75b42473b6cbf33d12cf3948c35f5d669
nytouu <capitaine7312@gmail.com>
2022-12-03 11:55:14 +0100

    remplacer getbyid par getbyclassname

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-player1 = document.getElementById("player1");
-player2 = document.getElementById("player2");
-player3 = document.getElementById("player3");
-player4 = document.getElementById("player4");
-
-const players = [];
-players.push(player1, player2, player3, player4);
-
-ennemy1 = document.getElementById("ennemy1");
-ennemy2 = document.getElementById("ennemy2");
-ennemy3 = document.getElementById("ennemy3");
-
-const ennemies = [];
-ennemies.push(ennemy1, ennemy2, ennemy3);
+players = document.getElementsByClassName("player")
+ennemies = document.getElementsByClassName("ennemy")
+buttons = document.getElementsByClassName("button")
 
 stats = document.getElementById("stats");
 
@@ -28,6 +17,7 @@ for(let i=0; i < players.length; i++) {
 			players[3].name = "Possum";
 	}
 	players[i].onmouseover = function() {
+        stats.style.color = "green"
 		stats.innerHTML = players[i].name + "<br> HP: " + players[i].hp;
 	}
 }
@@ -43,6 +33,7 @@ for(let i=0; i < ennemies.length; i++) {
 			ennemies[2].name = "Skeleton";
 	}
 	ennemies[i].onmouseover = function() {
+        stats.style.color = "red"
 		stats.innerHTML = ennemies[i].name + "<br> HP: " + ennemies[i].hp;
 	}
 }
