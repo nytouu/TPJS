a12d8fb0dd18ccdccbe5fa460c3d62eb701f3ea7
nytouu <capitaine7312@gmail.com>
2022-12-05 13:01:35 +0100

    les hp sont definis par entite

--- script.js
+++ script.js
@@ -9,16 +9,20 @@ let stats = document.getElementById("stats");
 
 // initialisation des joueurs
 for(let i=0; i < players.length; i++) {
-    players[i].hp = 20;
+    // selon leur index on leur donne un nom
 	switch(i) {
 		case 0:
 			players[0].name = "Magician";
+            players[0].hp = 15
 		case 1:
 			players[1].name = "Sara";
+            players[1].hp = 20
 		case 2:
 			players[2].name = "Bald";
+            players[2].hp = 30
 		case 3:
 			players[3].name = "Possum";
+            players[3].hp = 20
 	}
     // on defini les fonctions callback pour l'affichage des hp
 	players[i].onmouseover = function() {
@@ -32,14 +36,16 @@ for(let i=0; i < players.length; i++) {
 
 // meme chose pour les ennemis
 for(let i=0; i < ennemies.length; i++) {
-    ennemies[i].hp = 20;
 	switch(i) {
 		case 0:
 			ennemies[0].name = "Goblin";
+            ennemies[0].hp = 30;
 		case 1:
 			ennemies[1].name = "Spider";
+            ennemies[1].hp = 20;
 		case 2:
 			ennemies[2].name = "Skeleton";
+            ennemies[2].hp = 20;
 	}
 	ennemies[i].onmouseover = function() {
         stats.style.color = "red"
