907df7ad89452dfca5bb72492fe193a51ed34250
nytouu <capitaine7312@gmail.com>
2022-12-10 17:30:51 +0100

    tentative mecanique d'attaque semi fonctionnelle

--- script.js
+++ script.js
@@ -26,38 +26,43 @@ function getRandomInt(max)
     return Math.floor(Math.random() * max);
 }
 
-function highlightButtonPlayer(playersbuttons, selected)
+function clickButtonPlayer(playersbuttons, selectedButton, selectedPlayer)
 {
     // reset les couleurs sur tout les boutons
     for (let i=0; i < playersbuttons.length; i++)
     {
         playersbuttons[i].style.backgroundColor = "#65917B";
+        playersbuttons[i].selected = 0;
     }
-    if (selected.type == "player")
+    if (selectedButton.type == "player")
     {
-        selected.style.backgroundColor = "darkgreen";
+        selectedButton.style.backgroundColor = "darkgreen";
+        selectedPlayer.selected = 1;
     }
 }
 
-function highlightButtonEnnemy(ennemiesbuttons, selected)
+function clickButtonEnnemy(ennemiesbuttons, selectedButton, selectedEnnemy)
 {
     // reset les couleurs sur tout les boutons
     for (let i=0; i < ennemiesbuttons.length; i++)
     {
         ennemiesbuttons[i].style.backgroundColor = "#65917B";
+        ennemiesbuttons[i].selected = 0;
     }
-    if (selected.type == "ennemy")
+    if (selectedButton.type == "ennemy")
     {
-        selected.style.backgroundColor = "darkred";
+        selectedButton.style.backgroundColor = "darkred";
+        selectedEnnemy.selected = 1;
     }
 }
 
-function highlightButtonAction(selected)
+function clickButtonAction(selected)
 {
     // reset les couleurs sur tout les boutons
     for (let i=0; i < actionbuttons.length; i++)
     {
         actionbuttons[i].style.backgroundColor = "#65917B";
+        actionbuttons[i].selected = 0;
     }
 
 	// on colore le bouton selon son type
@@ -65,30 +70,57 @@ function highlightButtonAction(selected)
 	{
 		case 'attack':
 			selected.style.backgroundColor = "#ff0000";
+            selected.selected = 1;
 			break;
 		case 'defend':
 			selected.style.backgroundColor = "#0000ff";
+            selected.selected = 1;
 			break;
 		case 'special':
 			selected.style.backgroundColor = "#00ff00";
+            selected.selected = 1;
 			break;
     }
 }
 
-function highlightButtonReset(buttons)
+function highlightButtonResetAll(buttons)
 {
     for (let i=0; i < buttons.length; i++)
     {
         buttons[i].backgroundColor = "#65917B";
     }
+    for (let i=0; i < actionbuttons.length; i++)
+    {
+        actionbuttons[i].style.backgroundColor = "#65917B";
+    }
+}
+
+function resetSelection(players,ennemies,buttons)
+{
+    for (let i=0; i < players.length; i++)
+    {
+        players[i].selected = 0
+    }
+    for (let i=0; i < ennemies.length; i++)
+    {
+        ennemies[i].selected = 0
+    }
+    for (let i=0; i < buttons.length; i++)
+    {
+        buttons[i].selected = 0
+    }
 }
 
 function playerAttack(player,ennemy)
 {
+    console.log(player.state)
+    console.log(ennemy.state)
     if (player.state == "alive" && ennemy.state == "alive")
     {
+        console.log("both alive")
         if (player.mana > 0)
         {
+            console.log("has mana")
             printText(player.name + " is attacking " + ennemy.name);
             player.mana = player.mana - 10;
 
@@ -106,6 +138,35 @@ function playerAttack(player,ennemy)
         else if (player.mana <= 0)
         {
             printText(player.name + " couldn't attack, they didn't have enough mana !")
+            player.mana = player.max_mana;
+        }
+
+    }
+}
+
+function playerSpecial(player,ennemy)
+{
+    if (player.state == "alive" && ennemy.state == "alive")
+    {
+        if (player.mana > 20)
+        {
+            printText(player.name + " is attacking " + ennemy.name);
+            player.mana = player.mana - 20;
+
+            if (ennemy.hp > 0)
+            {
+                ennemy.hp = ennemy.hp - 20
+            }
+            if (ennemy.hp <= 0)
+            {
+                ennemy.hp = 0
+                printText(ennemy.name + " was defeated by " + player.name)
+                ennemy.state = "dead"
+            }
+        }
+        else if (player.mana <= 0)
+        {
+            printText(player.name + " couldn't make their special move, they didn't have enough mana !")
             player.mana = player.max_mana;
         }
 
@@ -145,6 +206,60 @@ function ennemyAttack(ennemy,player)
 function playerDefend(player)
 {
     player.state = "defending"
+    player.selected = 1
+}
+
+function playAction(players,ennemies,actions)
+{
+    let selectedPlayer = 0;
+    let selectedEnnemy = 0;
+    let selectedAction = 0;
+
+    console.log("button action")
+
+    for(let i=0; i < players.length; i++)
+    {
+        if (players[i].selected == 1)
+        {
+            selectedPlayer = players[i];
+            console.log("selected player : " + selectedPlayer.name)
+        }
+    }
+        for(let i=0; i < ennemies.length; i++)
+    {
+        if (ennemies[i].selected == 1)
+        {
+            selectedEnnemy = ennemies[i];
+            console.log("selected ennemy : " + selectedEnnemy.name)
+        }
+    }
+    for(let i=0; i < actions.length; i++)
+    {
+        if (actions[i].selected == 1)
+        {
+            selectedAction = actions[i];
+            console.log("selected action : " + selectedAction.type)
+        }
+    }
+    console.log(selectedPlayer + selectedEnnemy + selectedAction)
+    if (selectedPlayer != 0 && selectedEnnemy != 0 && selectedAction != 0)
+    {
+        switch (selectedAction.type)
+        {
+            case "attack":
+                playerAttack(selectedPlayer,selectedEnnemy)
+                console.log("attack triggered")
+                break;
+            case "defend":
+                playerDefend(selectedPlayer)
+                console.log("defense triggered")
+                break;
+            case "special":
+                playerSpecial(selectedPlayer)
+                console.log("special triggered")
+                break;
+        }
+    }
 }
 
 //
@@ -163,6 +278,7 @@ for(let i=0; i < players.length; i++)
             players[0].mana = 100;
             players[0].max_mana = 100;
             players[0].state = "alive"
+            players[0].selected = 0
 			break;
 		case 1:
 			players[1].name = "Sara";
@@ -170,6 +286,7 @@ for(let i=0; i < players.length; i++)
             players[1].mana = 60;
             players[1].max_mana = 60;
             players[1].state = "alive"
+            players[1].selected = 0
 			break;
 		case 2:
 			players[2].name = "Bald";
@@ -177,6 +294,7 @@ for(let i=0; i < players.length; i++)
             players[2].mana = 30;
             players[2].max_mana = 30;
             players[2].state = "alive"
+            players[2].selected = 0
 			break;
 		case 3:
 			players[3].name = "Possum";
@@ -184,6 +302,7 @@ for(let i=0; i < players.length; i++)
             players[3].mana = 0;
             players[3].max_mana = 0;
             players[3].state = "alive"
+            players[3].selected = 0
 			break;
 	}
 
@@ -208,16 +327,19 @@ for(let i=0; i < ennemies.length; i++)
 			ennemies[0].name = "Goblin";
             ennemies[0].hp = 40;
             ennemies[0].state = "alive";
+            ennemies[0].selected = 0
 			break;
 		case 1:
 			ennemies[1].name = "Spider";
             ennemies[1].hp = 30;
             ennemies[1].state = "alive";
+            ennemies[1].selected = 0
 			break;
 		case 2:
 			ennemies[2].name = "Skeleton";
             ennemies[2].hp = 30;
             ennemies[2].state = "alive";
+            ennemies[2].selected = 0
 			break;
 	}
 
@@ -253,7 +375,7 @@ for(let i=0; i < buttons.length; i++)
 		}
         buttons[i].onclick = function()
         {
-            highlightButtonPlayer(arr.filter(button => button.type == "player"), buttons[i]);
+            clickButtonPlayer(arr.filter(button => button.type == "player"), buttons[i],players[i]);
         }
 	}
     // puis les boutons ennemis
@@ -273,7 +395,7 @@ for(let i=0; i < buttons.length; i++)
 		}
         buttons[i].onclick = function()
         {
-            highlightButtonEnnemy(arr.filter(button => button.type == "ennemy"), buttons[i]);
+            clickButtonEnnemy(arr.filter(button => button.type == "ennemy"), buttons[i],ennemies[i-players.length]);
         }
 	}
 }
@@ -285,17 +407,30 @@ for(let i=0; i < actionbuttons.length; i++)
 	{
 		case 0:
 			actionbuttons[0].type = "attack";
+			actionbuttons[0].selected = 0;
             actionbuttons[0].onclick = function()
             {
-                highlightButtonAction(actionbuttons[i]);
-                playerAttack(players[1],ennemies[1])
+                clickButtonAction(actionbuttons[i]);
+                playAction(players,ennemies,actionbuttons)
             }
 			break;
 		case 1:
 			actionbuttons[1].type = "defend";
+			actionbuttons[1].selected = 0;
+            actionbuttons[1].onclick = function()
+            {
+                clickButtonAction(actionbuttons[i]);
+                playAction(players,ennemies,actionbuttons)
+            }
 			break;
 		case 2:
 			actionbuttons[2].type = "special";
+			actionbuttons[2].selected = 0;
+            actionbuttons[2].onclick = function()
+            {
+                clickButtonAction(actionbuttons[i]);
+                playAction(players,ennemies,actionbuttons)
+            }
 			break;
 	}
 }
